aca15eb86d176f5c165bc01a8bc1fcd3af5fbe69
mitchellsz231 <mitchellszeto@D-10-19-22-170.dhcp4.washington.edu>
2018-09-27 13:27:29 -0700

    program 3

--- Program-3/Program-3.ino
+++ Program-3/Program-3.ino
@@ -1,9 +1,9 @@
 /*
   Lab 1
-  Program 1
+  Program 3
   Mitchell Szeto, Feifan Qiao, and Bert Zhao
   
-  Turns an LED on for two second, then off for one second, repeatedly.
+  Turns the external LED Brick on for two second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
   it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
@@ -24,6 +24,8 @@
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
+int LED = 8; //define the 8th digital pin for LED brick
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -32,8 +34,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED, HIGH);           // turn the LED on (HIGH is the voltage level)
   delay(2000);                       // wait for two second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED, LOW);            // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for one second
 }
